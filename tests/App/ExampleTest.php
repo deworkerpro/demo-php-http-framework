e188dcbc483f8624251e4b19b1f49ec4e58fd083
ElisDN <mail@elisdn.ru>
2021-11-25 17:40:44 +0300

    Add example class

---
+++ tests/App/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\App;
+
+use App\Example;
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $example = new Example();
+
+        $result = $example->do();
+
+        self::assertEquals(42, $result);
+    }
+}
