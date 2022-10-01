dee334beef41729b976beae2fe78f5df58bd7a79
ElisDN <mail@elisdn.ru>
2022-10-01 13:33:18 +0300

    Add validation

--- public/index.php
+++ public/index.php
@@ -2,7 +2,10 @@
 
 declare(strict_types=1);
 
-/** @var string $name */
 $name = $_GET['name'] ?? 'Guest';
+
+if (!is_string($name)) {
+    exit;
+}
 
 echo '<h1>Hello, ' . htmlspecialchars($name) . '!</h1>';
