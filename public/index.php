7d2b9bf0f5b5221225d06c16642222fa82e23846
ElisDN <mail@elisdn.ru>
2022-10-01 13:33:21 +0300

    Fix error HTTP status

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+http_response_code(500);
+
 $name = $_GET['name'] ?? 'Guest';
 
 if (!is_string($name)) {
@@ -9,6 +11,7 @@ if (!is_string($name)) {
     exit;
 }
 
+http_response_code(200);
 header('Content-Type: text/plain; charset=utf-8');
 header('X-Frame-Options: DENY');
 echo 'Hello, ' . $name . '!';
