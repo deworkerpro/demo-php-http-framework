7235b2e27f4f18809523d56b932988e186de2e03
ElisDN <mail@elisdn.ru>
2022-10-01 13:33:22 +0300

    Add params tests

--- tests/App/DetectLangTest.php
+++ tests/App/DetectLangTest.php
@@ -15,8 +15,31 @@ final class DetectLangTest extends TestCase
 {
     public function testDefault(): void
     {
+        $_GET = [];
+        $_POST = [];
+
         $lang = detectLang('en');
 
         self::assertEquals('en', $lang);
+    }
+
+    public function testQueryParam(): void
+    {
+        $_GET = ['lang' => 'de'];
+        $_POST = ['lang' => 'fr'];
+
+        $lang = detectLang('en');
+
+        self::assertEquals('de', $lang);
+    }
+
+    public function testBodyParam(): void
+    {
+        $_GET = [];
+        $_POST = ['lang' => 'fr'];
+
+        $lang = detectLang('en');
+
+        self::assertEquals('fr', $lang);
     }
 }
