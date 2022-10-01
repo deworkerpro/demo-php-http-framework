c5f7853362252515170acba8410f5303daeb1922
ElisDN <mail@elisdn.ru>
2022-10-01 13:33:30 +0300

    Extract page code

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Framework\Http\Message\ServerRequest;
+
 use function App\detectLang;
 use function Framework\Http\createServerRequestFromGlobals;
 
@@ -10,18 +12,23 @@ http_response_code(500);
 /** @psalm-suppress MissingFile */
 require __DIR__ . '/../vendor/autoload.php';
 
-$request = createServerRequestFromGlobals();
+function home(ServerRequest $request): void
+{
+    $name = $request->getQueryParams()['name'] ?? 'Guest';
 
-$name = $request->getQueryParams()['name'] ?? 'Guest';
+    if (!is_string($name)) {
+        http_response_code(400);
+        return;
+    }
 
-if (!is_string($name)) {
-    http_response_code(400);
-    exit;
+    $lang = detectLang($request, 'en');
+
+    http_response_code(200);
+    header('Content-Type: text/plain; charset=utf-8');
+    header('X-Frame-Options: DENY');
+    echo 'Hello, ' . $name . '! Your lang is ' . $lang;
 }
 
-$lang = detectLang($request, 'en');
+$request = createServerRequestFromGlobals();
 
-http_response_code(200);
-header('Content-Type: text/plain; charset=utf-8');
-header('X-Frame-Options: DENY');
-echo 'Hello, ' . $name . '! Your lang is ' . $lang;
+home($request);
