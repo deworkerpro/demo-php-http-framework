e5a762c990a27620e29dc8ff7e640cdda6e07069
ElisDN <mail@elisdn.ru>
2022-10-01 13:33:27 +0300

    Fill headers

--- src/Framework/Http/createServerRequestFromGlobals.php
+++ src/Framework/Http/createServerRequestFromGlobals.php
@@ -8,17 +8,24 @@ use Framework\Http\Message\ServerRequest;
 
 function createServerRequestFromGlobals(): ServerRequest
 {
+    $headers = [
+        'Content-Type' => $_SERVER['CONTENT_TYPE'],
+        'Content-Length' => $_SERVER['CONTENT_LENGTH'],
+    ];
+
+    foreach ($_SERVER as $serverName => $serverValue) {
+        if (str_starts_with($serverName, 'HTTP_')) {
+            $name = ucwords(strtolower(str_replace('_', '-', substr($serverName, 5))), '-');
+            $headers[$name] = $serverValue;
+        }
+    }
+
     return new ServerRequest(
         serverParams: $_SERVER,
         uri: $_SERVER['REQUEST_URI'],
         method: $_SERVER['REQUEST_METHOD'],
         queryParams: $_GET,
-        headers: [
-            'Host' => $_SERVER['HTTP_HOST'],
-            'Content-Type' => $_SERVER['CONTENT_TYPE'],
-            'Content-Length' => $_SERVER['CONTENT_LENGTH'],
-            'Accept-Language' => $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '',
-        ],
+        headers: $headers,
         cookieParams: $_COOKIE,
         body: file_get_contents('php://input'),
         parsedBody: $_POST ?: null
