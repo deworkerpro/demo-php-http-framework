5e276c4813e0fc7890f7f899b8b6cbd2b90b7901
ElisDN <mail@elisdn.ru>
2022-10-01 13:33:21 +0300

    Add validation HTTP status

--- public/index.php
+++ public/index.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 $name = $_GET['name'] ?? 'Guest';
 
 if (!is_string($name)) {
+    http_response_code(400);
     exit;
 }
 
 header('Content-Type: text/plain; charset=utf-8');
 header('X-Frame-Options: DENY');
-
 echo 'Hello, ' . $name . '!';
