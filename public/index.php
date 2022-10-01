581c7d23eb222c4f47a519008f58f8170fa4640f
ElisDN <mail@elisdn.ru>
2022-10-01 13:33:24 +0300

    Pass request data

--- public/index.php
+++ public/index.php
@@ -5,40 +5,49 @@ declare(strict_types=1);
 http_response_code(500);
 
 /**
- * @param array<string, array|string> $query
- * @param array<string, array|string> $body
- * @param array<string, string> $cookie
- * @param array<string, string> $server
+ * @param array{
+ *     queryParams: array<string, string|array>,
+ *     parsedBody: array<string, string|array>|null,
+ *     cookieParams: array<string, string>,
+ *     serverParams: array<string, string>
+ * } $request
  */
-function detectLang(array $query, array $body, array $cookie, array $server, string $default): string
+function detectLang(array $request, string $default): string
 {
-    if (!empty($query['lang']) && is_string($query['lang'])) {
-        return $query['lang'];
+    if (!empty($request['queryParams']['lang']) && is_string($request['queryParams']['lang'])) {
+        return $request['queryParams']['lang'];
     }
 
-    if (!empty($body['lang']) && is_string($body['lang'])) {
-        return $body['lang'];
+    if (!empty($request['parsedBody']['lang']) && is_string($request['parsedBody']['lang'])) {
+        return $request['parsedBody']['lang'];
     }
 
-    if (!empty($cookie['lang'])) {
-        return $cookie['lang'];
+    if (!empty($request['cookieParams']['lang'])) {
+        return $request['cookieParams']['lang'];
     }
 
-    if (!empty($server['HTTP_ACCEPT_LANGUAGE'])) {
-        return substr($server['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    if (!empty($request['serverParams']['HTTP_ACCEPT_LANGUAGE'])) {
+        return substr($request['serverParams']['HTTP_ACCEPT_LANGUAGE'], 0, 2);
     }
 
     return $default;
 }
 
-$name = $_GET['name'] ?? 'Guest';
+$request = [
+    'serverParams' => $_SERVER,
+    'queryParams' => $_GET,
+    'cookieParams' => $_COOKIE,
+    'parsedBody' => $_POST ?: null,
+];
+
+$name = $request['queryParams']['name'] ?? 'Guest';
 
 if (!is_string($name)) {
     http_response_code(400);
     exit;
 }
 
-$lang = detectLang($_GET, $_POST, $_COOKIE, $_SERVER, 'en');
+$lang = detectLang($request, 'en');
 
 http_response_code(200);
 header('Content-Type: text/plain; charset=utf-8');
